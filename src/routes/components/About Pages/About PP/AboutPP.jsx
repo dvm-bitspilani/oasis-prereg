6d10297d5cb8d408d643788531481381d086888e
Akshit <f20220429@pilani.bits-pilani.ac.in>
2023-07-29 01:13:56 +0530

    added buttons to about pages

--- src/routes/components/About Pages/About PP/AboutPP.jsx
+++ src/routes/components/About Pages/About PP/AboutPP.jsx
@@ -4,17 +4,20 @@ import "../about.css"
 import bgImage from "../../../../assets/rectangle2.png"
 
 
-export default function AboutPP(){
-    return(
+export default function AboutPP() {
+    return (
         <>
-        <div className="page-container" style={{
-            backgroundImage : `url(${bgImage})`}}>
+            <div className="page-container" style={{
+                backgroundImage: `url(${bgImage})`
+            }}>
                 <div className="about-container">
                     <div className="about-title">About Purple Prose</div>
                     <div className="about-text">BITS, Pilani brings to you, 'Purple Prose : The Poetry Slam', a one of a kind platform for poets across the world to give their emotions, a thought and their thoughts, a word. With a chance to entertain student community of India's one of the largest institute, Purple Prose promises to take creative potential of an individual to national scale. So, breathe in experience and breathe out poetry.</div>
-                    <button className="about-preregister-button">Preregister</button>
-                </div>
-        </div>
+                    <div className="about-buttons-container">
+                        <button className="about-preregister-button">Register Now</button>
+                        <button className="about-preregister-button">Pay Now</button></div>                
+                        </div>
+            </div>
         </>
     )
 }
