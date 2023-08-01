bfac32fc117bdb0544d91657d007576a5568a4f7
Akshit <f20220429@pilani.bits-pilani.ac.in>
2023-08-01 15:34:56 +0530

    buid: conatct page

--- src/routes/components/About Pages/Contact.jsx
+++ src/routes/components/About Pages/Contact.jsx
@@ -1,5 +1,24 @@
-export default function Contact(){
-    return(
-        <div>hi</div>
-    )
+import React from "react";
+import "./about.css";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import Navbar from "./../Navbar/Navbar";
+
+export default function AboutPP() {
+    return (
+        <>
+            <Navbar />
+            <div className="page-container">
+                <div className="about-container">
+                    <div className="about-title">Contact</div>
+                    <div className="about-text contact">
+                        <div className="contact-container">
+                            <div className="person-details"><p>Irvit</p><p>+919811358655</p></div><p className="contact-events">SOAPBOX</p></div>
+                            <div className="contact-container">
+                        <div className="person-details"><p>Abhigyan Sharma</p><p>+916388489257</p></div><p className="contact-events">RAPWARS | PURPLE PROSE | SOAPBOX</p></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
