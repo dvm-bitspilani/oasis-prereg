0f545df74ea98018c70173265d42a1b6f2b54608
Animeshdj <animeshstick.567@gmail.com>
2024-07-24 22:39:41 +0530

    update: oasis logo

--- src/routes/components/Navbar/Navbar.jsx
+++ src/routes/components/Navbar/Navbar.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import '../Navbar/Navbar.css'
-import OasisLogo from '../../../assets/oasis-logo.png'
-import Oasis from '../../../assets/oasis.png'
-import ProfileImg from '../../../assets/profile.svg'
-import { Link } from 'react-router-dom'
-import BackBtn from '../../../assets/backarrow.png'
+import React from "react";
+import "../Navbar/Navbar.css";
+import Oasis from "../../../assets/oasis.png";
+import ProfileImg from "../../../assets/profile.svg";
+import { Link } from "react-router-dom";
+import BackBtn from "../../../assets/backarrow.png";
 const Navbar = () => {
   // const history = useHistory();
 
@@ -14,20 +13,28 @@ const Navbar = () => {
     window.history.back(); // Go back to the previous page
   };
   return (
-    <div className="navbar-container" >
+    <div className="navbar-container">
       <div className="navbar-back-button" onClick={handleBackButtonClick}>
-        <img src={BackBtn} width='10px' height='auto'></img>
+        <img src={BackBtn} width="10px" height="auto"></img>
       </div>
-        <div className="navbar-logo">
-            <Link to='/home' style={{textDecoration: "none"}}><img src={OasisLogo} alt="Oasis Logo" /></Link>
-            <Link to='/home' style={{textDecoration: "none"}}><div className="oasis-container"><img src={Oasis} alt="Oasis"/></div></Link>
-        </div>
-        <div className="navbar-items">
-            <Link to='/contact' style={{textDecoration: "none"}}><span>Contact</span></Link>
-            {/* <Link to='/profile' style={{textDecoration: "none"}}><img src={ProfileImg}></img></Link> */}
-        </div>
+      <div className="navbar-logo">
+        {/* <Link to="/home" style={{ textDecoration: "none" }}>
+          <img src={OasisLogo} alt="Oasis Logo" />
+        </Link> */}
+        <Link to="/home" style={{ textDecoration: "none" }}>
+          <div className="oasis-container">
+            <img src={Oasis} alt="Oasis" />
+          </div>
+        </Link>
+      </div>
+      <div className="navbar-items">
+        <Link to="/contact" style={{ textDecoration: "none" }}>
+          <span>Contact</span>
+        </Link>
+        {/* <Link to='/profile' style={{textDecoration: "none"}}><img src={ProfileImg}></img></Link> */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
