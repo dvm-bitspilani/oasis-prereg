51d16451969dde9943f5fff93eaadf26302c20d3
Ishaan02-Coder <ishaandaftari2@gmail.com>
2025-07-26 18:29:14 +0530

    changed payment confirmation

--- src/routes/components/PaymentPage/PaymentConfirmation.jsx
+++ src/routes/components/PaymentPage/PaymentConfirmation.jsx
@@ -1,9 +1,21 @@
 import { useEffect, useState } from "react";
-
+import { Link } from "react-router-dom";
 const PaymentConfirmation = () => {
   return(
     <div style={{ textAlign: "center", marginTop: "100px", fontSize: "24px" }}>
-      <h1>Payment Successful</h1>
+      <h1 style={{color:"white"}}>Payment Successful</h1>
+      <Link to="/home">
+        <button
+          style={{
+            marginTop: "20px",
+            padding: "10px 20px",
+            fontSize: "16px",
+            cursor: "pointer",
+          }}
+        >
+          Go to Home
+        </button>
+      </Link>
     </div>
   )
 };
